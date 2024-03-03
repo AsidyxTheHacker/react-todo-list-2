cc406e0091b229c5d8da184a6f378a0739f926b7
Adam <adammiller.2000nj@gmail.com>
2024-03-03 14:53:06 -0500

    added TodoItem component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import TodoItem from './TodoItem';
 
 export default function Todo() {
     const [todo, setTodo] = useState("");
@@ -16,7 +17,9 @@ export default function Todo() {
                <input onChange={(e) => setTodo(e.target.value)} value={todo} type="text" />
                <button type='submit'>Add</button>
            </form>
-           {console.log(todos)}
+           {todos.map(item => (
+            <TodoItem key={item} item={item} />
+           ))}
         </div>
     </>)
 };
